a409634d140e694cdd488936bccfa700379aa4a2
Sam-Lalo <lalo.sam2005@gmail.com>
2025-12-08 17:53:27 +0100

    v2 abs
    Added new functions for address handling and routing.

--- routeur_simple.ads
+++ routeur_simple.ads
@@ -1,22 +1,35 @@
+with LCA;
 package Routeur is
 
-	type T_LCA is limited private;
+	type T_Adresse is private;
+	type T_Routage is private;
 
-	-- Initialiser une Sda.  La Sda est vide.
+	-- Attribue une interface à partir d'une table de routage, et d'une adresse
 	procedure Attribuer_Interface (Adresse : T_Adresse, Table_Routage : T_Routage) with
 		Post => ...;
 		Pre => ...;
 
-
+	-- Calcule la longueur du masque, c'est à dire à partir de quel bit est a 0  
 	function Longueur_Masque (Adresse: T_Adresse) return Integer
+		Post => Result > 0 AND Result <= 32;
+
+	-- Verifie que les deux adresses données sont iddentique, identique à un octet pre ou rien de cela
+	function Adresse_Identique (Adresse1 : T_Adresse, Adresse2 : T_IP_Masque) return Condition; 
+		Post => (Adresse1 = Adresse2.Destination AND Result = Condition1) 
+					OR (Adresse1 != Adresse2.Destination AND Result = Aucune)
+					OR ();
+		Pre => ...;
+
+	-- Vérifie que notre adresse appartient à ce masque réseau
+	function Appartenir_Masque (Adresse : T_Adresse, Masque : T_Adresse) return Condition;
 		Post => ...;
 		Pre => ...;
 
-	function Adresse_Identique (Adresse1 : T_Adresse, Adresse2 : T_Adresse) return Condition; 
+-- Récupére le dernière octet, la position a laquelle le masque deviens nul  
+	function Recuperation_Derniere_Octet (Masque : T_Adresse, Adresse : T_Adresse) return Integer
 		Post => ...;
 		Pre => ...;
-	
-	function Appartenir_Masque (Adresse
+
 private
 
 		type T_IP_Masque is 
