797958705c18988008040d0746b3e300f64981b9
Sam-Lalo <lalo.sam2005@gmail.com>
2025-12-09 15:47:47 +0100

    correction Attribuer_Interface et remplissage Appartenir_Masque
    Refactor IP address handling and mask conditions

--- Routeur.adb
+++ Routeur.adb
@@ -10,48 +10,40 @@ package body Routeur is
 		end T_Potentiel;
 		Curseur_TR : T_Routage;
 		Potentiel : T_Potentiel;
-		IP_Identique : T_Condition
-		Appartenir_Masque : T_Condition
+		Appartenir_Masque : Boolean;
 	begin
 		Curseur_TR := Table_Routage;
 		Potentiel := T_Potentiel (Longueur = 0, Interface = "eth0");
 		While Curseur_TR /= null loop
 		
-			-- Récupération des conditions utiles aux traitements des données
-			IP_Identique := Adresse_Identique (Adresse, Curseur_TR.all.Cle);
-			Appartenir_Masque := Appartenir_Masque(Adresse, Curseur_TR.all.Cle.Masque);
-			
-			-- Traitement de l'adresse IP pour voir si elle pourrait correspondre
-			if IP_Identique = Condition1 and Longueur > Potentiel.Longueur then
-				Potentiel := Potentiel (Longueur = Longueu, Interface = Curseur_TR.Valeur);
-			elsif IP_Identique = Condition1bis and Appartenir_Masque = Condition2 and Longueur > Potentiel.Longueur then
-				Potentiel := Potentiel (Longueur = Longueu, Interface = Curseur_TR.Valeur);
+			-- L'adresse appartient tel au masque ? 
+			Appartenir_Masque := Appartenir_Masque (Adresse, Curseur_TR.all.Cle);
+			Longuer := Longueur_Masque (Curseur_TR.all.Cle.Masque);
+
+			-- Verifie que l'adresse est correspond mieux que la precedente
+			if Appartenir_Masque and Longueur > Potentiel.Longueur then
+				Potentiel := Potentiel (Longueur = Longueur, Interface = Curseur_TR.Valeur);
+			else
+				null;
 			end if;
 			
 			Curseur_TR := Curseur_TR.all.Suivant;
 		end loop;
 		
 		-- écriture des réponses dans le fichier résultats
-		Put (Resultat, Curseur_Adresse.all.Valeur);
+		Put (Resultat, Adresse);
 		Put (Resultat, “ “ & Potentiel.Interface );
 		New_Line;
 	end Attribuer_Interface;
-	
-	function 
 	
 	function Longueur_Masque (Adresse: T_Adresse) return Integer is
 	begin
 		return 0;
 	end Longueur_Masque;
 	
-	function Adresse_Identique (Adresse1 : T_Adresse, Adresse2 : T_IP_Masque) return Condition is
+	function Appartenir_Masque (Adresse : T_Adresse, Adresse_Routeur : T_IP_Masque) return Boolean is
 	begin
-		return Aucune;
-	end Adresse_Identique;
-	
-	function Appartenir_Masque (Adresse : T_Adresse, Masque : T_Adresse) return Condition is
-	begin
-		return Aucune;
+		return (Adresse and Adresse_Routeur.Masque) = Adresse_Routeur.Destination;
 	end Appartenir_Masque;
 	
 	function Recuperation_Dernire_Octet (Masque : T_Adresse, Adresse : T_Adresse) return Integer
